e9382abe953e38bc3c4716f249393414a441cef2
misticniVrac <karashinc@gmail.com>
2020-03-04 23:11:44 +0100

    started working on textures

---
+++ src/graphics/texture2d.cpp
@@ -0,0 +1,41 @@
+#include "../utils/stb_image.h"
+#include "texture2d.h"
+
+#include <iostream>
+
+
+#include <GL/glew.h>
+
+
+namespace spasoje{
+
+
+
+Texture2D::Texture2D(const char *path)
+{
+
+	unsigned char *data = stbi_load(path, &(this->m_width), &(this->m_height), &(this->m_channels), 0);
+	
+
+	if(!data)
+	{
+		std::cout << "failed to load texture " << path << "\n";
+		return;
+	}
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, this->m_width, this->m_height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	stbi_image_free(data);
+
+
+}
+
+int
+Texture2D::getID()
+{
+	return this->ID;
+}
+
+}
+
---
+++ src/graphics/texture2d.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "../utils/stb_image.h"
+
+namespace spasoje{
+
+class Texture2D
+{
+
+private:
+	unsigned int ID;	
+	int m_width,m_height,m_channels;
+public:
+	Texture2D(const char *path);
+	
+	int getID();
+
+};
+
+}
