0bc90e9bd7c3ee48e37f4a5883613a520f9af83f
misticniVrac <karashinc@gmail.com>
2020-03-12 21:41:37 +0100

    included lua

---
+++ src/luainc.h
@@ -0,0 +1,11 @@
+#ifndef __LUA_INC_H__
+#define __LUA_INC_H__
+
+extern "C"
+{
+	   #include "lualib/src/lua.h"
+	   #include "lualib/src/lauxlib.h"
+	   #include "lualib/src/lualib.h"
+}
+
+#endif  //__LUA_INC_H__
